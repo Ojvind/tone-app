b7473e5afe57b736d00c955d00a2fd0085152e9a
Öjvind Otterbjörk <ojvind.otterbjork@icloud.com>
2025-05-22 11:13:18 +0200

    Bryt ut toner till en fil med konstanter

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,7 @@ import NoteDisplay from './NoteDisplay';
 import GuessInput from './GuessInput';
 import ScoreBoard from './ScoreBoard';
 import GameOver from './GameOver';
-
-const TREBLE_NOTES = [
-  { name: 'C', key: 'c/4' }, { name: 'D', key: 'd/4' }, { name: 'E', key: 'e/4' },
-  { name: 'F', key: 'f/4' }, { name: 'G', key: 'g/4' }, { name: 'A', key: 'a/4' },
-  { name: 'B', key: 'b/4' }, { name: 'C', key: 'c/5' },
-];
-
-const BASS_NOTES = [
-  { name: 'F', key: 'f/2' }, { name: 'G', key: 'g/2' }, { name: 'A', key: 'a/2' },
-  { name: 'B', key: 'b/2' }, { name: 'C', key: 'c/3' }, { name: 'D', key: 'd/3' },
-  { name: 'E', key: 'e/3' }, { name: 'F', key: 'f/3' }, { name: 'G', key: 'g/3' },
-  { name: 'A', key: 'a/3' }, { name: 'B', key: 'b/3' }, { name: 'C', key: 'c/4' },
-];
+import { TREBLE_NOTES, BASS_NOTES } from './constants';
 
 function App() {
   const [notes, setNotes] = useState([]);
